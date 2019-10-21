3ec3d97df5621152756d02cab6bf5108d8194f70
ShadwDrgn <c.arcane1@gmail.com>
2019-10-20 22:33:27 -0400

    make it so it can export i-i.com json for now

--- pf_init.py
+++ pf_init.py
@@ -3,6 +3,7 @@ import requests
 import json
 import tkinter
 from tkinter import Tk
+from bs4 import BeautifulSoup
 
 class UI():
     def __init__(self):
@@ -28,7 +29,28 @@ class UI():
         self.list_monsters = tkinter.Listbox(self.frame_main,exportselection=False)
         self.list_monsters.grid(column=1,row=1,sticky='wns')
         self.list_type.bind('<<ListboxSelect>>',self.on_monster)
+        self.list_monsters.bind('<<ListboxSelect>>',self.on_monster_sel)
+        self.lbl_json = tkinter.Label(self.frame_main, text='JSON for improved-initiative.com').grid(row=3)
+        self.json_text = tkinter.Text(self.frame_main)
+        self.json_text.grid(row=4,columnspan=4,sticky='nsew')
 
+
+    def on_monster_sel(self,evt):
+        sel = evt.widget.curselection()
+        if not sel:
+            return
+        i = int(sel[0])
+        tval = evt.widget.get(i)
+        mon = self.find(tval,self.monsters)
+        print(mon)
+        cmon_json = json.dumps(convert_mon(mon))
+        self.json_text.delete(1.0,'end')
+        self.json_text.insert('end',cmon_json)
+
+    def find(self,fstr,monsters):
+        mon = [mon for mon in monsters if mon['name'].lower() == fstr.lower()]
+        if len(mon) > 0:
+            return mon[0]
 
     def on_monster(self,evt):
         sel = evt.widget.curselection()
@@ -76,6 +98,90 @@ def load_data(file):
         with open(file,'r', encoding='utf8') as f:
             data=json.loads(f.read())
     return data
+
+
+def get_hitdice(mon):
+    res = requests.get(mon['link']).text
+    soup = BeautifulSoup(res, 'html.parser')
+    stat_list = soup.find_all('p', class_='stat-block-1')
+    hp = [val for val in soup.find_all('p', class_='stat-block-1') if 'hp' in val.text][0].text.split(' ')[2]
+    return hp
+
+
+def get_skills(mon):
+    skills = list()
+    mskills = mon['skills'][7:].split(', ')
+    for skill in mskills:
+        skill_name = skill.split(' ')[0]
+        skill_value = skill.split(' ')[1]
+        skills.append({ 'Name': skill_name, 'Modifier': skill_value})
+    return skills
+
+def get_actions(mon):
+    atts = mon['meleeAttacks']
+    atts.extend(mon['rangedAttacks'])
+    actions = list()
+    for att in atts:
+        name = att.split('+')[0].strip()
+        content = att
+        actions.append({'Name': name, 'Content': content, 'Usage':''})
+    return actions
+
+def convert_mon(mon):
+    return {
+      "Source": "Pathfinder",
+      "Type": "",
+      "HP": {
+        "Value": int(mon['hp']),
+        "Notes": get_hitdice(mon)
+      },
+      "AC": {
+        "Value": mon['ac'],
+        "Notes": ""
+      },
+      "InitiativeModifier": mon['init'],
+      "InitiativeAdvantage": False,
+      "Speed": [],
+      "Abilities": {
+        "Str": mon['strength'],
+        "Dex": mon['dexterity'],
+        "Con": mon['constitution'],
+        "Int": mon['intelligence'],
+        "Wis": mon['wisdom'],
+        "Cha": mon['charisma']
+      },
+      "DamageVulnerabilities": [],
+      "DamageResistances": [],
+      "DamageImmunities": [],
+      "ConditionImmunities": [],
+      "Saves": [
+        {
+          "Name": "Fortitude",
+          "Modifier": mon['fort']
+        },
+        {
+          "Name": "Reflex",
+          "Modifier": mon['ref']
+        },
+        {
+          "Name": "Will",
+          "Modifier": mon['will']
+        }
+      ],
+      "Skills": get_skills(mon),
+      "Senses": mon['senses'],
+      "Languages": mon['languages'],
+      "Challenge": mon['cr'],
+      "Traits": [],
+      "Actions": get_actions(mon),
+      "Reactions": [],
+      "LegendaryActions": [],
+      "Description": "",
+      "Player": "player",
+      "Version": "2.9.2",
+      "ImageURL": ""
+    }
+
 
 
 def main():
